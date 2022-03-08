d6527606aa002bb6f2b92064340053d84cf7a0f7
Alison8547 <alisonailson100@hotmail.com>
2022-03-07 22:04:20 -0300

    Strategy

--- src/one/digitalinnovation/gof/Testes.java
+++ src/one/digitalinnovation/gof/Testes.java
@@ -1,7 +1,13 @@
 package one.digitalinnovation.gof;
 
+import one.digitalinnovation.gof.singleton.SingletonEager;
+import one.digitalinnovation.gof.singleton.SingletonLazy;
+import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
+import one.digitalinnovation.gof.strategy.*;
+
 public class Testes {
     public static void main(String[] args) {
+        // Singleton
         SingletonLazy lazy = SingletonLazy.getInstance();
         System.out.println(lazy);
         System.out.println(lazy);
@@ -13,5 +19,24 @@ public class Testes {
         SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstance();
         System.out.println(lazyHolder);
         System.out.println(lazyHolder);
+
+        // Strategy
+
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.setComportamento(defensivo);
+        robo.mover();
+        robo.mover();
+        robo.setComportamento(agressivo);
+        robo.mover();
+        robo.mover();
+        robo.mover();
+
+
     }
 }
